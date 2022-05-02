fb3cc92e2982b991669893a3661cd32d4daf9e6e
ATUL GUPTA <akgnit7@gmail.com>
2022-05-03 00:00:15 +0530

    Update 6ImageBrightnessCompost.py

--- 6ImageBrightnessCompost.py
+++ 6ImageBrightnessCompost.py
@@ -4,6 +4,9 @@ from PIL import Image
 file = 'binance.jpeg'
 image = Image.open(file)
 
+# To Apply the Filters, we need to convert the image to RGB mode
+image = image.convert('RGB')
+
 print('{}x{}'.format(image.width, image.height))
 
 # Changing the brigthness of the image and compositing them one above another in a contact sheet

05dad57f98fd558b5eb291c26987156328b4a43d
ATUL GUPTA <akgnit7@gmail.com>
2022-05-02 23:38:32 +0530

    Update 4CroppingImage.py

--- 4CroppingImage.py
+++ 4CroppingImage.py
@@ -4,6 +4,9 @@ from PIL import Image
 file = 'binance.jpeg'
 image = Image.open(file)
 
+# To Apply the Filters, we need to convert the image to RGB mode
+image = image.convert('RGB')
+
 print('{}x{}'.format(image.width, image.height))
 
 print('** Cropping Image (left, upper, right, lower) **')
